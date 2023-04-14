d4d35fa0275c4b93533f39e192c920843dc66f97
NomnomPlays <104179573+NomnomPlays@users.noreply.github.com>
2023-04-14 08:29:10 -0500

    Add files via upload

---
+++ past_announcements.md
@@ -0,0 +1,7 @@
+## This will be updated with every announcement made in the banner with dates.
+
+### The announcement leaves at 11:59pm CDT/CST on the end date.
+
+|         Dates         |     Announcement     |
+|-----------------------|----------------------|
+|04/04/2023 - 04/10/2023|I am 17 years old now!|
---
+++ script.js
@@ -0,0 +1,135 @@
+// Set birth date and time as variables
+var birthDatetime = new Date("2006-04-03T15:02:00");
+var birthTime = "3:02 PM";
+
+// Get the current date and time
+var now = new Date();
+
+// Calculate difference in years between birthdate and current date
+var ageDiff = now.getTime() - birthDatetime.getTime();
+var ageDate = new Date(ageDiff);
+var currentYear = ageDate.getUTCFullYear();
+var age = currentYear - 1970;
+
+// If birth date is after current date, subtract 1 from the age
+var birthMonth = birthDatetime.getMonth() + 1; // Add 1 because getMonth() returns 0-based index
+var birthDay = birthDatetime.getDate();
+var currentMonth = now.getMonth() + 1; // Add 1 because getMonth() returns 0-based index
+var currentDay = now.getDate();
+
+if (currentMonth < birthMonth || (currentMonth == birthMonth && currentDay < birthDay)) {
+  age--;
+}
+  
+function printResults(){
+  var message = "I am a "+age+" year old programmer that somewhat knows how to program.<br>I can use some basic Java and Python but not Javascript.";
+  if (window.location.pathname == '/' || window.location.pathname == '/index.html') {
+    document.getElementById('age').innerHTML = message;
+  }
+}
+
+printResults();
+
+var announcement = "I am 17 years old now!";
+// Get the body element
+var body = document.getElementsByTagName("body")[0];
+
+// Create the topbar element
+var topbar = document.createElement("div");
+topbar.setAttribute("id", "banner");
+topbar.textContent = "Announcement: " + announcement;
+
+// Add the topbar element to the body
+body.insertBefore(topbar, body.firstChild);
+
+var startDateTime = new Date("2023-04-04T00:00:00");
+var endDateTime = new Date("2023-04-10T23:59:59");
+
+endDateTime = endDateTime.getTime();
+startDateTime = startDateTime.getTime();
+curTime = now.getTime();
+
+toggleTopbar();
+
+// Call the hideTopbar function every second
+setInterval(toggleTopbar, 1000);
+
+function toggleTopbar() {
+  if (curTime > endDateTime || curTime < startDateTime) {
+    topbar.style.display = "none";
+  } else {
+    topbar.style.display = "block";
+  }
+  var now = new Date();
+  curTime = now.getTime();
+  // alert("End Date:      " + endDateTime + "\nCurrent Time:  " + curTime + "\nStart Date:    " + startDateTime);
+}
+
+
+function showDiscordPopup() {
+  var discordPopup = document.getElementById("discord-popup");
+  discordPopup.classList.toggle("show");
+}
+
+if (window.location.pathname == '/settings.html') {
+  var lightBtn = document.getElementById('light');
+  var darkBtn = document.getElementById('dark');
+  var oledBtn = document.getElementById('oled');
+  var rad = document.themes.theme;
+  var prev = null;
+  var clicks = 0;
+  
+  for (var i = 0; i < rad.length; i++) {
+    rad[i].addEventListener('change', function() {
+      (prev) ? console.log(prev.value): null;
+      if (this !== prev) {
+          prev = this;
+      }
+      if (lightBtn && lightBtn.checked) {
+        document.documentElement.classList.remove("dark");
+        document.documentElement.classList.remove("oled");
+        document.documentElement.classList.add("light");
+        window.localStorage.setItem('mode', 'light');
+      } else if (darkBtn && darkBtn.checked) {
+        document.documentElement.classList.remove("light");
+        document.documentElement.classList.remove("oled");
+        document.documentElement.classList.add("dark");
+        window.localStorage.setItem('mode', 'dark');
+      } else if (oledBtn && oledBtn.checked) {
+        document.documentElement.classList.remove("light");
+        document.documentElement.classList.remove("dark");
+        document.documentElement.classList.add("oled");
+        window.localStorage.setItem('mode', 'oled');
+      }
+    })
+  }
+}
+
+var theme = window.localStorage.getItem('mode');
+
+if (theme == 'dark') {
+  if (window.location.pathname == '/settings.html') {
+    darkBtn.checked = true;
+  }
+  document.documentElement.classList.remove("light");
+  document.documentElement.classList.remove("oled");
+  document.documentElement.classList.add("dark");
+}
+
+if (theme == 'light') {
+  if (window.location.pathname == '/settings.html') {
+    lightBtn.checked = true;
+  }
+  document.documentElement.classList.remove("dark");
+  document.documentElement.classList.remove("oled");
+  document.documentElement.classList.add("light");
+}
+
+if (theme == 'oled') {
+  if (window.location.pathname == '/settings.html') {
+    oledBtn.checked = true;
+  }
+  document.documentElement.classList.remove("dark");
+  document.documentElement.classList.remove("light");
+  document.documentElement.classList.add("oled");
+}
